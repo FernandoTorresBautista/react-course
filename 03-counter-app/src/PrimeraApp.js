891d249c40e5d6f6c25a80cfce0daa9376569ad6
FernandoTorresBautista <fernandot371@gmail.com>
2022-02-06 16:00:39 -0600

    sec 4 - 39,40,41,42,43,44,45 props, proptypes, defaultprops, counterapp

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import React, { Fragment } from 'react';
 
 // Functional components
-const PrimeraApp = ( { saludo = 'Hola mundo' } ) => {
-
-
+const PrimeraApp = ( { saludo, subtitulo } ) => {
+    // no es buena opcion
+    // if ( !saludo ) {
+    //     throw new Error('El saludo es necesario')
+    // }
 
     return (
         <>
             <h1> { saludo } </h1>
-            <p>Mi primera aplicación</p>
+            <p>{ subtitulo }</p>
         </>
     );
 
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 
 export default PrimeraApp;
