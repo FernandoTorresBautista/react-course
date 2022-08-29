2820811f3c1ce0026b21a29fa882c875479d1585
FernandoTorresBautista <fernandot371@gmail.com>
2022-08-29 18:53:10 -0500

    events & hook useState

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp title="Fernando" subTitle={123} />
+        <CounterApp value={10} />
     </React.StrictMode>
 );
