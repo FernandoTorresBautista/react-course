80bdd9c4ebe4c48c9939430ad41b9c517a55be37
FernandoTorresBautista <fernandot371@gmail.com>
2022-08-29 18:21:34 -0500

    using PropTypes

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types';
 
-export const FirstApp = ( {title, subtitle} ) => {
+export const FirstApp = ( {title, subTitle} ) => {
   return (
     <>
         <h1>{ title }</h1>
         <p>{ subtitle }</p>
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired,
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -7,6 +7,6 @@ import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp title="Fernando" subtitle={123} />
+        <FirstApp title="Fernando" subTitle={123} />
     </React.StrictMode>
 );
