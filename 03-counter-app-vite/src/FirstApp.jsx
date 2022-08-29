7b91f3d53297d88f5b2073e8d9863c664ca1d874
FernandoTorresBautista <fernandot371@gmail.com>
2022-08-29 18:15:29 -0500

    communication between components using props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,25 +1,9 @@
 
-// we can put the constants inside the component but 
-// they would be reprocessed when the component autoreloads
-const GetName = () => {
-  // return a + b;
-  return <h1>"Fernando"</h1>
-}
-
-// const newMessage = "Fernando"; // [0,1,2,3,4,5,6,7,8,9], true
-// const newMessage = {} // objects are not valid like React child
-const newMessage = {
-  name:  'Fernando',
-  age: 27,
-}
-
-export const FirstApp = () => {
+export const FirstApp = ( {title, subtitle} ) => {
   return (
     <>
-        <code>{ JSON.stringify( newMessage ) }</code> 
-        <p>Soy un subtitulo</p>
-        <h3>{ 1 * 5 }</h3>
-        <h3>{ GetName() }</h3>
+        <h1>{ title }</h1>
+        <p>{ subtitle }</p>
     </>
   )
 }
