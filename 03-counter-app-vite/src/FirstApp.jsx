baf5e6d3f34be753c4750af608ea24f5d2dc1e2b
FernandoTorresBautista <fernandot371@gmail.com>
2022-08-29 18:27:30 -0500

    default props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ( {title, subTitle} ) => {
+export const FirstApp = ( {title, subTitle, name} ) => {
   return (
     <>
         <h1>{ title }</h1>
-        <p>{ subtitle }</p>
+        <p>{ subTitle }</p>
+        <p>{ name }</p>
     </>
   )
 }
@@ -12,4 +13,10 @@ export const FirstApp = ( {title, subTitle} ) => {
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
   subTitle: PropTypes.number.isRequired,
+}
+
+FirstApp.defaultProps = {
+  name: "FT",
+  title: 'No hay título',
+  subTitle: 123,
 }
