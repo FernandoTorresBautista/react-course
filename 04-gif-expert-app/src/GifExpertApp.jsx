f95996a150f712e6966c7bd223f4df0bde1d13e1
FernandoTorresBautista <fernandot371@gmail.com>
2022-09-29 15:16:27 -0500

    adding categories using the useState from the parent element

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        {/* Title */}
-        <h1>GifExpertApp</h1>
-        {/* Input */}
-        {/* List of items(gifs) */}
+
+    const [categories, setCategories] = useState(['One Punch']);
+    // console.log(categories);
+
+    return (
+        <>
+            {/* Title */}
+            <h1>GifExpertApp</h1>
+            {/* Input */}
+            <AddCategory setCategories={setCategories} />
+            {/* List of items(gifs) */}
+            <ol>
+                { 
+                    categories.map( categorie => {
+                        return <li key={ categorie }>{categorie}</li>
+                    }) 
+                }
+            </ol>
             {/* GifItem */}
-    </>
-  )
+        </>
+    )
 }
