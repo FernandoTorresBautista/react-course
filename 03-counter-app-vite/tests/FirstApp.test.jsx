ff980ca1c7dc7d62bb8d7bc4515e2f5e399efef2
FernandoTorresBautista <fernandot371@gmail.com>
2022-09-29 13:32:57 -0500

    manipulate the container with javascript, it is like a DOM

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -6,7 +6,23 @@ describe('Pruebas en <FirstApp />', () => {
     test('should match with the snapshot', () => {
         
         const title = 'Hola, soy Goku';
-        render( <FirstApp title={ title } /> );
+        const {container} = render( <FirstApp title={ title } /> );
+
+        // eval the entire html
+        expect( container ).toMatchSnapshot();
+
+    });
+
+    test("should show the title in a tag H1", () => {
+
+        const title = 'Hola, soy Goku';
+        const {container, getByText} = render( <FirstApp title={ title } /> );
+
+        expect( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toBe( title );
+        expect(h1.innerHTML).toContain( title ); // if we have spaces in the tag
 
     });
 
