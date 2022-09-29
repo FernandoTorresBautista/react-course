55a0166d410a2eca98badd6584dcee449bccd52f
FernandoTorresBautista <fernandot371@gmail.com>
2022-09-29 13:40:39 -0500

    getByTestId and other props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export const FirstApp = ( {title, subTitle, name} ) => {
   return (
     <>
-        <h1>{ title }</h1>
+        <h1 data-testid="test-title">{ title }</h1>
         <p>{ subTitle }</p>
         <p>{ name }</p>
     </>
@@ -12,11 +12,11 @@ export const FirstApp = ( {title, subTitle, name} ) => {
 
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
-  subTitle: PropTypes.number.isRequired,
+  subTitle: PropTypes.string,
 }
 
 FirstApp.defaultProps = {
   name: "FT",
   // title: 'No hay título',
-  subTitle: 123,
+  subTitle: "123",
 }
--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -3,26 +3,43 @@ import { FirstApp } from '../src/FirstApp';
 
 describe('Pruebas en <FirstApp />', () => {
     
-    test('should match with the snapshot', () => {
+    // test('should match with the snapshot', () => {
         
-        const title = 'Hola, soy Goku';
-        const {container} = render( <FirstApp title={ title } /> );
+    //     const title = 'Hola, soy Goku';
+    //     const {container} = render( <FirstApp title={ title } /> );
 
-        // eval the entire html
-        expect( container ).toMatchSnapshot();
+    //     // eval the entire html
+    //     expect( container ).toMatchSnapshot();
 
-    });
+    // });
 
     test("should show the title in a tag H1", () => {
 
         const title = 'Hola, soy Goku';
-        const {container, getByText} = render( <FirstApp title={ title } /> );
+        const {container, getByText, getByTestId} = render( <FirstApp title={ title } /> );
 
         expect( getByText(title) ).toBeTruthy();
 
-        const h1 = container.querySelector('h1');
-        expect(h1.innerHTML).toBe( title );
-        expect(h1.innerHTML).toContain( title ); // if we have spaces in the tag
+        // const h1 = container.querySelector('h1');
+        // expect(h1.innerHTML).toBe( title );
+        // expect(h1.innerHTML).toContain( title ); // if we have spaces in the tag
+
+        expect( getByTestId('test-title').innerHTML ).toBe( title );
+        expect( getByTestId('test-title').innerHTML ).toContain( title );
+
+    });
+
+    test('should show the subtitle sending for us', () => {
+        
+        const title = 'Hola, soy Goku';
+        const subtitle = 'Soy un subtitulo';
+        const {getByText} = render( 
+            <FirstApp 
+                title={ title } 
+                subTitle={ subtitle } 
+            /> 
+        );
+        expect( getByText(subtitle) ).toBeTruthy();
 
     });
 
