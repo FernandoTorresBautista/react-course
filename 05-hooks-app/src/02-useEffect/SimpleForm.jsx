673b2626e3f9a60f29b5c6560a70c1b8b951c325
FernandoTorresBautista <fernandot371@gmail.com>
2022-12-28 12:14:15 -0600

    sec9 - useEffect - simpleForm, dependencias

---
+++ 05-hooks-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'example',
+        email: 'username@example.com'
+    })
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    }
+
+    useEffect(() => {
+        console.log('useEffect called!');
+    }, []); // [] executed one time
+
+
+    useEffect(() => {
+        console.log('formState changed!');
+    }, [formState]); // [] executed one time
+    
+    useEffect(() => {
+        console.log('email changed!');
+    }, [email]); // [] executed one time
+
+    return (
+        <>
+            <h1>Formulario simple</h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="username@example.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+        </>
+    )
+}
--- 05-hooks-app/src/main.jsx
+++ 05-hooks-app/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { HooksApp } from './HooksApp'
 
@@ -9,6 +10,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterWithCustomHook />
+    <SimpleForm />
   </React.StrictMode>,
 )
