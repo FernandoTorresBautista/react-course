2e3bf0610c33a35ebe177f02aced67a63bb6b089
FernandoTorresBautista <fernandot371@gmail.com>
2022-12-28 11:51:00 -0600

    useState counter customhook

---
+++ 05-hooks-app/src/hooks/useCounter.js
@@ -0,0 +1,30 @@
+// custom hook
+// hook it's just a function 
+
+import { useState } from "react"
+
+export const useCounter = (initialValue= 10) => {
+    // 
+    const [counter, setCounter] = useState(initialValue)
+
+    const increment = (value = 1) => {
+        setCounter(counter + value)
+    }
+
+    const decrement = (value = 1) => {
+        // if (counter  === 0) return; // more than 0
+        setCounter(counter - value)
+    }
+
+    const reset = () => {
+        setCounter(initialValue)
+    }
+
+    return {
+        counter,
+        increment,
+        decrement, 
+        reset,
+    }
+
+}
