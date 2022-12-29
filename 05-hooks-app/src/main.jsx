f58bd594c6716c480a610575d9af3a0843f81a3a
FernandoTorresBautista <fernandot371@gmail.com>
2022-12-28 18:16:08 -0600

    sec9 - useMemo

--- 05-hooks-app/src/main.jsx
+++ 05-hooks-app/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Layout } from './05-useLayourtEffect/Layout'
+import { MemoHook } from './06-memos/MemoHook'
 
+// import { Memorize } from './06-memos/Memorize'
+// import { Layout } from './05-useLayourtEffect/Layout'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
@@ -15,6 +17,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <MemoHook />
   </React.StrictMode>,
 )
