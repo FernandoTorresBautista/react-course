b8ae96273836a61e3e0c797c01c5591ba19072f7
FernandoTorresBautista <fernandot371@gmail.com>
2022-12-29 13:09:46 -0600

    sec10 - guardar y leer en localstorage

--- 05-hooks-app/src/08-useReducer/TodoApp.jsx
+++ 05-hooks-app/src/08-useReducer/TodoApp.jsx
@@ -1,25 +1,30 @@
+import { useEffect } from "react";
 import { useReducer } from "react"
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
 import { todoReducer } from "./todoReducer"
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del alma',
-        done: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del tiempo',
-        done: false,
-    },
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false,
+    // }
 ]
+
+// inicizliza los valores si estan en el local storage
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
 
-    // const [state, dispatch] = useReducer(todoReducer, initialState);
-    const [todos, dispatchTodo] = useReducer(todoReducer, initialState);
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+    //disparar un efecto secundario - useEffect
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])    
 
     const hadleNewTodo = (todo) => {
         const addTodoAction = {
