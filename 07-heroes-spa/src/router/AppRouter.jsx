840a2817bdff0e04b825bc31a5180bf86385ef6e
FernandoTorresBautista <fernandot371@gmail.com>
2022-12-30 13:48:59 -0600

    sec14 - create first router and active path

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,17 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "../auth/pages/LoginPage"
-import { DCPage } from "../heroes/pages/DCPage"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
+ import { Navigate, Route, Routes } from "react-router-dom";
+
+ 
+ import { DCPage, MarvelPage } from "../heroes";
+ import { LoginPage } from "../auth";
+ import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-        <Route path="/marvel" element={<MarvelPage />} />
-        <Route path="/dc" element={<DCPage />} />
+    <>
+        <Navbar />
+        <Routes>
+            <Route path="/marvel" element={<MarvelPage />} />
+            <Route path="/dc" element={<DCPage />} />
 
-        <Route path="/login" element={<LoginPage />} />
-        
-        <Route path="/*" element={<Navigate to="/marvel" />} />
-    </Routes>
+            <Route path="/login" element={<LoginPage />} />
+            
+            <Route path="/*" element={<Navigate to="/marvel" />} />
+        </Routes>
+    </>
   )
 }
